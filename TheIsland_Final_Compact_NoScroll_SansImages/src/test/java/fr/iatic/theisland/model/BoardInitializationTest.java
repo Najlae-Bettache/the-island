72dccbfce763fc8e36edfe52833558a44888ae73
Najlae-Bettache <najlae.bettache13@gmail.com>
2026-05-28 00:09:23 +0200

    Update BoardInitializationTest.java

--- TheIsland_Final_Compact_NoScroll_SansImages/src/test/java/fr/iatic/theisland/model/BoardInitializationTest.java
+++ TheIsland_Final_Compact_NoScroll_SansImages/src/test/java/fr/iatic/theisland/model/BoardInitializationTest.java
@@ -6,9 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-/**
- * Tests du plateau initial.
- */
+
 class BoardInitializationTest {
 
     @Test
