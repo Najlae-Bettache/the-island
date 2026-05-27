3fc436ac27af432fc3333912057dcfaf4f1a4253
Najlae-Bettache <najlae.bettache13@gmail.com>
2026-05-28 00:10:28 +0200

    Update InitialPlacementServiceTest.java

--- TheIsland_Final_Compact_NoScroll_SansImages/src/test/java/fr/iatic/theisland/setup/InitialPlacementServiceTest.java
+++ TheIsland_Final_Compact_NoScroll_SansImages/src/test/java/fr/iatic/theisland/setup/InitialPlacementServiceTest.java
@@ -12,9 +12,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-/**
- * Tests de préparation de partie.
- */
+
 class InitialPlacementServiceTest {
 
     @Test
