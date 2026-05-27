aabba3bf6ffffb24695c1bae74df335e3b86bc1d
Najlae-Bettache <najlae.bettache13@gmail.com>
2026-05-28 00:09:49 +0200

    Update MovementServiceTest.java

--- TheIsland_Final_Compact_NoScroll_SansImages/src/test/java/fr/iatic/theisland/movement/MovementServiceTest.java
+++ TheIsland_Final_Compact_NoScroll_SansImages/src/test/java/fr/iatic/theisland/movement/MovementServiceTest.java
@@ -18,9 +18,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-/**
- * Tests des règles de déplacement.
- */
+
 class MovementServiceTest {
 
     @Test
